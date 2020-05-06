4c5ca86613eb6ce8a22955321bfaa129520a8213
jere95 <62517951+jere95@users.noreply.github.com>
2020-05-06 18:40:53 -0300

    Resolución completa
    Resolución reto 3

---
+++ reto 3/reto 3/src/reto3/Main.java
@@ -0,0 +1,19 @@
+package reto3;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Platzi cuenta con cursos de: \n DESARROLLO E INGENIERÍA \n DISEÑO Y UX \n MARKETING \n NEGOCIOS Y EMPRENDIMIENTO \n PRODUCCIÓN AUDIOVISUAL \n CRECIMIENTO PROFESIONAL");
+
+	}
+
+
+}
+
+
+
+
+
